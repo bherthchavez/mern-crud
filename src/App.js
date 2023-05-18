4cf99382b8f5435e9d7ddd8dacb8d1f269c90d52
bherth <bherthchavez@gmail.com>
2023-05-18 23:24:26 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import List from "./components/List";
+import axios from "axios";
 
-function App() {
+const App = () => {
+  const [input, setInput] = useState("");
+  const [tasks, setTasks] = useState([]);
+  const [updateUI, setUpdateUI] = useState(false);
+  const [updateId, setUpdateId] = useState(null);
+
+  useEffect(() => {
+    axios.get(`http://localhost:5000/api/get`).then((res) => {
+      // console.log(res.data);
+      setTasks(res.data);
+    });
+  }, [updateUI]);
+
+  const addTask = () => {
+    axios.post(`http://localhost:5000/api/save`, { task: input }).then((res) => {
+      // console.log(res.data);
+      setInput("");
+      setUpdateUI((prevState) => !prevState);
+    });
+  };
+
+  const updateMode = (id, text) => {
+    console.log(text);
+    setInput(text);
+    setUpdateId(id);
+  };
+
+  const updateTask = () => {
+    axios.put(`http://localhost:5000/api/update/${updateId}`, { task: input }).then((res) => {
+      // console.log(res.data);
+      setUpdateUI((prevState) => !prevState);
+      setUpdateId(null);
+      setInput("");
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="p-4  sm:p-6">
+      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+        <h2 className="mt-10 text-center text-2xl font-bold text-gray-900">
+          CRUD
+        </h2>
+        <div className="mt-2 flex items-center justify-between gap-3" >
+
+          <input
+            type="text"
+            required
+            className="block w-full rounded-md  border-0 py-1.5 p-4 text-gray-900  ring-1  ring-gray-300 placeholder:text-gray-400  sm:text-lg sm:leading-6"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+          />
+
+          <button
+            type="submit"
+            className="w-40 justify-center rounded-md bg-gray-600  py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-gray-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            onClick={updateId ? updateTask : addTask}
+          >
+            {updateId ? "Update Task" : "Add Task"}
+          </button>
+        </div>
+
+
+        <ul className="list-none mt-5">
+          {tasks.map((task) => (
+            <List
+              key={task._id}
+              id={task._id}
+              task={task.task}
+              setUpdateUI={setUpdateUI}
+              updateMode={updateMode}
+            />
+          ))}
+        </ul>
+      </div>
+
+    </main>
   );
-}
+};
 
 export default App;
---
+++ src/components/List.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { BsTrash } from "react-icons/bs";
+import { BiEditAlt } from "react-icons/bi";
+import axios from "axios";
+
+const List = ({ id, task, setUpdateUI, updateMode }) => {
+  const removeTask = () => {
+    axios.delete(`http://localhost:5000/api/delete/${id}`).then((res) => {
+      // console.log(res);
+      setUpdateUI((prevState) => !prevState);
+    });
+  };
+
+  return (
+    <li className="flex justify-between p-5 mb-5 bg-slate-100 rounded-lg">
+      {task}
+      <div className="flex gap-5 justify-center">
+        <BiEditAlt title="Edit" className="cursor-pointer text-gray-600" size={20} onClick={() => updateMode(id, task)} />
+        <BsTrash title="Delete" className="cursor-pointer text-gray-600" size={20} onClick={removeTask} />
+      </div>
+    </li>
+  );
+};
+
+export default List;
